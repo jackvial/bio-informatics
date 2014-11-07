dcb902a227e81d603dd3a0c4d2c50523a168fe29
Jack Vial <vialjack@mail.com>
2014-11-06 22:36:23 -0500

    Code challenge 1 solved

--- patterncount.js
+++ patterncount.js
@@ -1,11 +1,9 @@
-// A function for finding the frequency of given pattern in a given text
-var testText = 'ATACGCGCGATACGCGATACGCGATAATACGCGACACATA';
-
 
 function patternCount(text, pattern){
+
 	var count = 0;
 	
-	for(var i = 0; i < text.length - pattern.length; i++){
+	for(var i = 0; i < text.length - (pattern.length - 1); i++){
 		//console.log(text.substr(i, pattern.length));	
 		if(text.substr(i, pattern.length) === pattern){
 			count++;
@@ -15,4 +13,24 @@ function patternCount(text, pattern){
 	return count;
 }
 
-console.log(patternCount(testText, 'ATA'));
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+  process.exit(1);
+}
+
+// Read the file and print its contents.
+var fs = require('fs')
+  , filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+
+  console.log(patternCount(data, 'ACTGGCAAC'));
+  //console.log('OK: ' + filename);
+  //console.log(data)
+});
+
+
+
+//console.log(patternCount(testText, 'GCG'));
