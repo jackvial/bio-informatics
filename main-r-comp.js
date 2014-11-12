97a711da7a643a49f27066864be28246c9a18f2a
Jack Vial <vialjack@mail.com>
2014-11-12 13:46:13 -0500

    Reverse Complement Tests passing

--- main-r-comp.js
+++ main-r-comp.js
@@ -3,14 +3,14 @@ var reverseComplement = require('./reversecomplement.js');
 
 var readline = require('readline');
 
+// Create an interface object
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Get input and process it
 rl.question("Enter a string of DNA: ", function(answer) {
-  // TODO: Log the answer in a database
   console.log("The reverse complement of the DNA string you enter is: :", reverseComplement(answer));
-
   rl.close();
 });
--- reversecomplement.js
+++ reversecomplement.js
@@ -1,5 +1,28 @@
 function reverseComplement(dnaString){
-  return dnaString;	
+
+  var reverseC = [];
+  var dnaArr = dnaString.split('');
+
+  for(var i = 0; i < dnaArr.length; i++){
+	
+	switch(dnaArr[i]){
+		case 'A':
+			reverseC.push('T');
+		break;
+		case 'T':
+			reverseC.push('A');
+		break;
+		case 'C':
+			reverseC.push('G');
+		break;
+		case 'G':
+			reverseC.push('C');
+		break;
+	}
+  }
+  
+  // Reverse and rejoin the the string	
+  return reverseC.reverse().join('');	
 }
 
 module.exports = reverseComplement;
